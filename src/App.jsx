87e864af26adef5cf6a14997bfa3e713bacc48c5
banderGlitch <nernaykumar98@gmail.com>
2024-04-29 23:02:26 +0530

    login register completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Home from "./pages/Home/Home"
+import Login from "./pages/Login/login"
+import Register from "./pages/Register/register"
 
 
 function App() {
   
   return ( 
     <>
-    <Home/>
+    <Register/>
+    {/* <Login/> */}
+    {/* <Home/> */}
     </>
   )
 }
